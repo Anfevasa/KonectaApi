339dddd6015eeea76bdd6572b2a4bfe9a884f686
diseño y desarrollo <disenoydesarrollo@academicos.cesde.edu.co>
2023-11-16 04:56:52 -0500

    primer commit

---
+++ src/main/java/com/example/proyectoSura/servicios/AfiliadoServicio.java
@@ -0,0 +1,56 @@
+package com.example.proyectoSura.servicios;
+
+import com.example.proyectoSura.entidades.Afiliado;
+import com.example.proyectoSura.repositorios.AfiliadoRepositorio;
+import com.example.proyectoSura.utilidades.Enum;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Optional;
+@Service
+public class AfiliadoServicio {
+    @Autowired
+    private AfiliadoRepositorio afiliadoRepositorio;
+
+    //REGISTRAR AFILIADO
+    public Afiliado registrarAfiliado(Afiliado afiliado)throws Exception{
+        try {
+            return this.afiliadoRepositorio.save(afiliado);
+        }catch (Exception error){
+            throw new Exception(Enum.ERROR_REGISTRO.getMensaje());
+        }
+    }
+
+    //CONSULTAR AFILIADOS
+    public Afiliado consultarAfiliado(Integer idAfiliado)throws Exception{
+        try {
+            Optional<Afiliado>afiliadoBuscado=this.afiliadoRepositorio.findById(idAfiliado);
+            if (afiliadoBuscado.isPresent()){ //AQUI ENTRA SI LO ENCUENTRA EN LA BD
+                return afiliadoBuscado.get();
+            }else { //SI NO LO ENCUENTRA LANZA LA EXCEPCION
+                throw new Exception(Enum.AFILIADO_INEXISTENTE.getMensaje());
+            }
+        }catch (Exception error){
+            throw new Exception(Enum.ERROR_EN_CONSULTA.getMensaje());
+        }
+    }
+    //
+    public List<Afiliado> consultarAfiliados()throws Exception{
+        try {
+            List<Afiliado> afiliados =this.afiliadoRepositorio.findAll();
+            if (!afiliados.isEmpty()){ //AQUI ENTRA SI LO ENCUENTRA EN LA BD
+                return afiliados;
+            }else { //SI NO LO ENCUENTRA LANZA LA EXCEPCION
+                throw new Exception(Enum.AFILIADO_INEXISTENTE.getMensaje());
+            }
+        }catch (Exception error){
+            throw new Exception(Enum.ERROR_EN_CONSULTA.getMensaje());
+        }
+    }
+
+    //MODIFICAR DATOS AFILIADOS
+    //BORRAR AFILIADO
+
+}
